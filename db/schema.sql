b571ec7328bd2030e3204e113237575de00749a2
Dan Aument <danaument@gmail.com>
2021-01-12 14:48:20 -0600

    created db files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROPDATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    burger_name VARCHAR(255);
+    devoured BOOLEAN
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Pete's Overblown BBQ Explosion Burger", false),
+("Actually Just Pickles Burger", false),
+("Pâté au Filet sur Brioche", false),
+("0.625 Pounder with Cheese", false);
