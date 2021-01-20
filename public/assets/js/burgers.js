a5f63e8a39c86cae8a8606ff80725dc7bb3eff69
Dan Aument <danaument@gmail.com>
2021-01-20 12:57:17 -0600

    added burgers.js. made slight changes to handlebars views to reflect intended button interactions

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,52 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+  $(".devour").on("click", function (event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newdevoured");
+
+    var newDevoured = {
+      devooured: true,
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevoured,
+    }).then(function () {
+      console.log("changed devoured to", newDevoured);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burger").val().trim(),
+      devoured: false,
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".delete-burger").on("click", function (event) {
+    let id = $(this).data("id");
+
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
