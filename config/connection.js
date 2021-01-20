829f223ea0ec69a1c491bcbfc71db2418279a5c3
Dan Aument <danaument@gmail.com>
2021-01-20 12:06:48 -0600

    refactored connection.js with promisify

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
+const util = require('util');
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "tester",
@@ -17,6 +18,8 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+connection.query = util.promisify(connection.query);
 
 // Export connection for our ORM to use.
 module.exports = connection;
