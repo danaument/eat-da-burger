e32d5fc61251c4359201bbafbfa63d4d9d73f836
Dan Aument <danaument@gmail.com>
2021-01-23 17:35:57 -0600

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,21 @@
 const mysql = require("mysql");
 const util = require('util');
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "tester",
-  password: "tester",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'tester',
+        user: 'tester',
+        password: '',
+        database: 'burgers_db'
+    })
+};
+
 
 // Make connection.
 connection.connect(function(err) {
